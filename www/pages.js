61045e93869b821c9d83c79cc32cb6b66d91eb82
Justin Reina <justinmreina@gmail.com>
2021-11-03 10:32:03 -0700

    (+)    add page reference meta - js

---
+++ www/pages.js
@@ -0,0 +1,93 @@
+/************************************************************************************************************************************/ 
+/* @file		  pages.js
+ * 	@brief		setup information for web pages
+ * 	@details	heights, colors & names
+ */
+/************************************************************************************************************************************/ 
+function tryMe() {
+  return "Justin!!";
+}
+
+/************************************************************************************************************************************/
+/* @fcn		    getPageSize(page) 							
+ * @brief			get the selected page's size    
+ * @details	  x                       				
+ * 																						
+ * @param		  [in] (string) page - page for selection         												                                       
+ */ 																													                                                               
+/************************************************************************************************************************************/ function getPageSize(page) {
+  
+  switch(page) {
+    case 'Home':
+      return '950px';
+    case "Embedded":
+      return '1200px';
+    case "Software":
+      return '1000px';
+    case "T&M Content":
+      return '400px';
+    case "Apps":
+      return '400px';
+    case "Smart Home":
+      return '400px';
+    case "RPM":
+      return '400px';
+    case "Helpful Things":
+      return '400px';
+    case "Dev":
+      return '500px';
+    case "Shared Sandbox":
+      return '400px';
+    case "Portfolio Short":
+      return '950px';
+    case "Contact":
+      return '1500px';
+    default:
+      alert("Error at getPageSize() for '" + page +"' selection");
+      return 'oops';
+  }
+}
+
+/************************************************************************************************************************************/
+/* @fcn		    getPageFileName(page)
+ * @brief			get the filename for selected page	
+ * @details	  x                       						
+ * 																								
+ * @param		  [in] (string) page - page for selection         												                                       
+ * 																													                                                                 
+ * @section   Opens																					                                                                 
+ * 		 condense data here to structs												                                                                 
+ *
+/************************************************************************************************************************************/ function getPageFileName(page) {
+  
+  switch(page) {
+    case 'Home':
+      return 'home.html';
+    case "Embedded":
+      return embedded.html';
+    case "Software":
+      return 'software.html';
+    case "T&M Content":
+      return 'test.html';
+    case "Apps":
+      return 'apps.html';
+    case "Smart Home":
+      return 'smarts.html';
+    case "RPM":
+      return 'proto.html';
+    case "Helpful Things":
+      return 'util.html';
+    case "Dev":
+      return 'dev.html';
+    case "Shared Sandbox":
+      return 'sandbox.html';
+    case "Portfolio Short":
+      return 'short.html';
+    case "Contact":
+      return 'contact.php';
+    default:
+      alert("Error at getPageFileName() for '" + page +"' selection");
+      return 'oops';
+  }
+}
+
