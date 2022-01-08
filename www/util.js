b6880e30d55f54377906fef3a71d2fb45f1c9e6b
Justin Reina <justinmreina@gmail.com>
2022-01-08 05:02:02 -0800

    (+)    publish favicon work

--- www/util.js
+++ www/util.js
@@ -48,7 +48,7 @@ function inputIsValid(field) {
 
 	//Contants
 	var supported = ["home", "embedded", "software", "apps", "smart", "proto", "troll", "util", "dev", "cows", "sandbox", "portfolio",
-									 "contact", "bing", "sub", "resume", "justSmile"];
+									 "contact", "bing", "sub", "resume", "justSmile", "favicon"];
 
   //Locals
   var isSupported;																													/* is input param supported?														*/
